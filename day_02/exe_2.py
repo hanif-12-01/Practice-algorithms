fbcf5e0de1f0c2f49f1e4f4f398f55999a51d543
hanif-12-01 <stshfaiz@gmail.com>
2026-06-22 16:29:01 +0700

    Buat refresh ingatan

---
+++ day_02/exe_2.py
@@ -0,0 +1,12 @@
+nilai=int(input("Masukkan nilai anda: "))
+
+if nilai >= 80:
+    print("Nilai anda A")
+elif nilai >= 70:
+    print("Nilai anda B")
+elif nilai >= 60:
+    print("Nilai anda C")
+elif nilai >= 50:
+    print("Nilai anda D")
+else:
+    print("Nilai anda E")
---
+++ day_02/exe_6.py
@@ -0,0 +1,8 @@
+sampah=int(input("Masukkan jumlah sampah: "))
+
+if sampah >=85:
+    print("Kirim truk sampah")
+elif sampah >=60:
+    print("Pantau kondisi sampah")
+else:
+    print("kondisi aman")
