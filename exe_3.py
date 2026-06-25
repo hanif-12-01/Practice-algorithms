20f96c57007ae5034a6cb11fc2a2d5bfb5a1e3bd
hanif-12-01 <stshfaiz@gmail.com>
2026-06-26 00:46:20 +0700

    hapus

--- exe_3.py
+++
@@ -1,7 +0,0 @@
-total=[]
-for i in range (5):
-    nilai=int(input("Masukkan nilai: "))
-    total.append(nilai)
-for i in total:
-    print("Nilai yang dimasukkan:", i)
-    print ("rata-rata nilai:", sum(total)/len(total)) 
